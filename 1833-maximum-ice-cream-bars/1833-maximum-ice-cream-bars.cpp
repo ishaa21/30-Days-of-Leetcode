daac55417679c967858e41eb11196ae7bd01305d
Isha Zalavadia <128834694+ishaa21@users.noreply.github.com>
2026-06-21 20:02:13 +0530

    Time: 8 ms (86.81%), Space: 84.4 MB (11.94%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int canBuy = 0;
-        for(int cost : costs)
+        int maxCost = *max_element(costs.begin(),costs.end());
+        vector<int> freq(maxCost + 1,0);
+        int count = 0;
+        for(int c : costs)
         {
-            if(coins >= cost)
-            {
-                coins -= cost;
-                canBuy++;
-            }
-            else
-            {
-                break;
-            }
-            
+            freq[c]++;
         }
-        return canBuy;
+
+        for(int cost = 1; cost <= maxCost; cost++)
+        {
+            if(freq[cost] == 0) continue;
+            int buy = min(freq[cost], coins/cost);
+            coins -= buy * cost;
+            count += buy;
+            if(coins < cost) break;
+        }
+        return count;
     }
 };
